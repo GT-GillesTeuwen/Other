e0bf312787f8ccaba8fe5057ae1e64c825102964
GT-GillesTeuwen <103629800+GT-GillesTeuwen@users.noreply.github.com>
2023-03-30 17:59:07 +0200

    first commit

---
+++ Test.java
@@ -0,0 +1,21 @@
+public class Test {
+    
+    private static boolean assertEquals(int expected,int actual){
+        if(expected==actual){
+            System.out.println("Passed");
+            return true;
+        }
+            System.out.println("Expected "+expected+" got "+actual);
+        return false;
+    }
+
+    public static void run(){
+        assertEquals(0, 0);
+        assertEquals(1, 0);
+    }
+
+    public static void main(String[] args) {
+        run();
+    }
+
+}
